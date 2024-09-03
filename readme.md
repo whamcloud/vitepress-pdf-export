495a3f626e55b42fe6340c2938ff813404aef208
Jonathan Parris <jparris@ddn.com>
2024-09-03 17:02:52 -0600

    Initial Page Number using Type 1 PDF fonts

--- readme.md
+++ readme.md
@@ -25,6 +25,20 @@ Key               | Description                                                 
 `url`             | VitePress URl.  e.g., `http://localhost:5173`                                                   |         | `String`
 `vitepress_links` | Paths to json file defining the url layout of the VitePress site                                |         | `Vec<PathBuf>`
 
+### page_number
+Key     | Description                                               | Type
+--------|-----------------------------------------------------------|--------------------------------------------------------
+`color` | RGB values between 0 and 1.0                              | TOML Table with keys `r`, `g`, `b` and values are `f64`
+`font`  | PDF Type 1 - see table below for options                  | `String`
+`size`  | Font size                                                 | `i16`
+`x`     | Page Number X offset (in inches) from the top left corner | `f64`
+`y`     | Page Number Y offset (in inches) from the top left corner | `f64`
+
+#### PDF Type 1 Fonts
+* `Times−Roman`, `Times−Bold`, `Times−Italic`, `Times−BoldItalic`,
+* `Helvetica`, `Helvetica−Bold`, `Helvetica−Oblique`, `Helvetica−BoldOblique`,
+* `Courier`, `Courier−Bold`, `Courier−Oblique`, `Courier−BoldOblique`
+
 ### print_to_pdf
 Yes underscore is the default case for TOML but these options come from Chrome DevTool Protocol which uses camel case. See [Chrome DevTool Protocol](https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-printToPDF) for additional options.
 Key                       | Description                                               | Type           | Chrome DevTool Protocol Default
@@ -40,4 +54,7 @@ Key                       | Description                                         
 
 ## Useful Dev Resources
 * [PDF 1.7 Spec](https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/PDF32000_2008.pdf)
+* [PDF Reference](https://opensource.adobe.com/dc-acrobat-sdk-docs/pdfstandards/pdfreference1.4.pdf)
 * *PDF Explained: The ISO Standard for Document Exchange* by John Whitington
+* [PDF Debugger](https://pdf.hyzyla.dev/): Web based, easier to navigate but doesn't have support for Page Contents.
+* [Interactive PDF Analysis](https://github.com/seekbytes/IPA): Native app that you have to compile, navigation is a little harder but it has support for Page Contents.
--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,7 @@
 // Copyright (c) 2024 DDN. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use headless_chrome::types::PrintToPdfOptions;
 use indexmap::{indexset, set::IndexSet};
 use serde::Deserialize;
@@ -13,6 +12,78 @@ use std::{fs, path::PathBuf};
 #[derive(Debug, Deserialize)]
 struct ConfigFile {
     pub config: Config,
+}
+
+/// Page Number Color
+#[derive(Debug, Deserialize)]
+pub struct Color {
+    pub r: f64,
+    pub g: f64,
+    pub b: f64,
+}
+
+impl Color {
+    fn valid(&self) -> Result<()> {
+        let mut invalid = vec![];
+        if self.r < 0.0 || self.r > 1.0 {
+            invalid.push("r");
+        }
+        if self.g < 0.0 || self.g > 1.0 {
+            invalid.push("g");
+        }
+        if self.b < 0.0 || self.b > 1.0 {
+            invalid.push("b");
+        }
+        if !invalid.is_empty() {
+            return Err(anyhow!(
+                "Channel values must for be in range 0.0 to 1.0. Invalid channel(s) {}",
+                invalid.join(",")
+            ));
+        }
+        Ok(())
+    }
+}
+
+/// Page Numbers Style
+#[derive(Debug, Deserialize)]
+pub struct PageNumber {
+    /// Font Color
+    pub color: Color,
+    /// Font Name
+    pub font: String,
+    /// Font size
+    pub size: i16,
+    /// Page Number X offset (in inches) from the top left corner
+    pub x: f64,
+    /// Page Number Y offset (in inches) from the top left corner
+    pub y: f64,
+}
+
+impl PageNumber {
+    fn valid(&self) -> Result<()> {
+        self.color.valid()?;
+        let type1_fonts = [
+            "Times−Roman",
+            "Times−Bold",
+            "Times−Italic",
+            "Times−BoldItalic",
+            "Helvetica",
+            "Helvetica−Bold",
+            "Helvetica−Oblique",
+            "Helvetica−BoldOblique",
+            "Courier",
+            "Courier−Bold",
+            "Courier−Oblique",
+            "Courier−BoldOblique",
+        ];
+        if !type1_fonts.contains(&self.font.as_str()) {
+            return Err(anyhow!(
+                "Invalid font name {}. Only PDF Type 1 Fonts are supported",
+                self.font
+            ));
+        }
+        Ok(())
+    }
 }
 
 /// We expect `vitepress-pdf-export` to be run as part of a CI actions so all options
@@ -33,6 +104,9 @@ pub struct Config {
     pub urls: IndexSet<String>,
     /// List of paths to JSON files that define the `VitePress` site.
     pub vitepress_links: Vec<PathBuf>,
+    /// Page Number Style - if not defined page numbers will not be inserted.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub page_number: Option<PageNumber>,
     /// PDF Generation options see [Chrome DevTool Protocol](https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-printToPDF) for documentation.
     pub print_to_pdf: PrintToPdfOptions,
 }
@@ -70,6 +144,10 @@ impl Config {
     /// Loads the TOML file and generates the list of URLS to render into PDFs
     pub fn load(path: &PathBuf) -> Result<Self> {
         let mut conf: Config = toml::from_str::<ConfigFile>(&fs::read_to_string(path)?)?.config;
+
+        if let Some(page_number) = &conf.page_number {
+            page_number.valid()?;
+        }
 
         let mut index = conf.url.clone();
         index.push_str("/index.html");
